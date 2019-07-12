fcbdba6057c3de92a401bafd3229856e28cfd9ca
SegFault42 <SegFault42@protonmail.com>
2019-07-12 23:24:05 +0200

    define values

--- include/print.h
+++ include/print.h
@@ -3,7 +3,12 @@
 
 #include "file.h"
 
-#define MAX_LINE	39
+#define HEADER_NB_LINE		3
+#define FOOTER_NB_LINE		3
+#define CONSOLE_MAX_LINE	45
+#define MAX_LINE			CONSOLE_MAX_LINE - (HEADER_NB_LINE + FOOTER_NB_LINE)
+
+// how many caracter can be printed until end of line (for filename)
 #define MAX_LINE_PRINT	74
 
 void	print_directory(t_files_select *begin, size_t cursor);
